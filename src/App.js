af24093d1c25e3bd50892c00f059ea6025c7f39e
Trojan1989 <emmanueldilag@gmail.com>
2021-10-22 11:30:00 +0800

    10/22/2021

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import TodoList from "./components/TodoList";
+import TodoInput from "./components/TodoInput";
+import "bootstrap/dist/css/bootstrap.min.css";
+import uuid from "uuid";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto col-md-8 mt-4">
+            <h3 className="text-center">Todo Input</h3>
+            <TodoInput></TodoInput>
+            <TodoList></TodoList>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
